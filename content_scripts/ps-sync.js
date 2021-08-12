07690d1755469d98d9308564f710fb96002c4f31
Martin Shi <martinshi1206@hotmail.com>
2021-08-13 01:14:11 +0800

    improve update action

--- content_scripts/ps-sync.js
+++ content_scripts/ps-sync.js
@@ -20,8 +20,20 @@
             return Promise.resolve({data: data});
         } else if (message.command === 'update') {
             let new_team = message.data;
-            window.wrappedJSObject.Storage.loadPackedTeams(new_team);
+            // update data in localStorage
             window.localStorage.setItem('showdown_teams', new_team);
+            window.wrappedJSObject.Storage.loadTeams();
+
+            // emulate click the button of selectFolder on all
+            let room = window.wrappedJSObject.room;
+            if (room.selectFolder) {
+              room.curFolderKeep ="";
+              room.curFolder ="";
+              room.updateFolderList();
+              room.updateTeamList(true);
+              
+              room.update();   // force update the data in room
+            }
         }
       });
 })();
